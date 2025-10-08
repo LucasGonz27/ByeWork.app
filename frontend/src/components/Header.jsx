45ea9f7d59c0fc6ade80ec3916eddfe626667e33
Baixerastheo <theo.baixeras@gmail.com>
2025-10-08 16:56:14 +0200

    Udpate du Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,25 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
+import Logo from '../assets/LogoByeWork.png';
 
 function Header() {
-  return (
-    <nav className={styles.header}>
-      <Link to="/">Accueil</Link>
-      <Link to="/offers">Offres</Link>
-      <Link to="/signup">Créer un compte</Link>
-    </nav>
-  );
+    return (
+        <nav className={styles.header}>
+            {/* Logo à gauche */}
+            <Link to="/" className={styles.logo}>
+                <img src={Logo} alt="Logo ByeWork" />
+            </Link>
+
+            {/* Conteneur pour les liens à droite */}
+            <div className={styles.navLinks}>
+                <Link to="/">Accueil</Link>
+                <Link to="/offers">Offres</Link>
+                <Link to="#">Trouver une entreprise</Link>
+                <Link to="/signup">Créer un compte</Link>
+                <Link to="#">Publier une offre</Link>
+            </div>
+        </nav>
+    );
 }
 
 export default Header;
